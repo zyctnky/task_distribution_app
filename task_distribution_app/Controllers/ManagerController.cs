685720e9d9ad76cda89289bb5313559279ec52a9
ZiyaCetinkaya <ziyacetinkaya@outlook.com>
2023-05-31 20:40:30 +0300

    Manager kullanıcısı için ekran tasarımları tamamlandı.

---
+++ task_distribution_app/Controllers/ManagerController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace task_distribution_app.Controllers
+{
+    [RoutePrefix("manager")]
+    public class ManagerController : Controller
+    {
+        [Route("")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("task/{id}/details")]
+        public ActionResult TaskDetails(int id)
+        {
+            return View();
+        }
+
+        [Route("task/{id}/edit")]
+        public ActionResult TaskEdit(int id)
+        {
+            return View();
+        }
+
+        [Route("task/{id}/delete")]
+        public ActionResult TaskDelete(int id)
+        {
+            return View();
+        }
+
+        [Route("developer")]
+        public ActionResult DeveloperList()
+        {
+            return View();
+        }
+
+        [Route("developer/{id}/details")]
+        public ActionResult DeveloperDetails(int id)
+        {
+            return View();
+        }
+    }
+}
