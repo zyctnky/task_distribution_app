ccc2a6d662650c5445acdaf9f7efef35655c2d36
ZiyaCetinkaya <ziyacetinkaya@outlook.com>
2023-05-31 20:50:43 +0300

    Analist kullanıcısı için ekran tasarımları tamamlandı.

---
+++ task_distribution_app/Controllers/AnalystController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace task_distribution_app.Controllers
+{
+    [RoutePrefix("analyst")]
+    public class AnalystController : Controller
+    {
+        [Route("")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("task/create")]
+        public ActionResult TaskCreate()
+        {
+            return View();
+        }
+
+        [Route("task/{id}/details")]
+        public ActionResult TaskDetails(int id)
+        {
+            return View();
+        }
+
+        [Route("task/{id}/edit")]
+        public ActionResult TaskEdit(int id)
+        {
+            return View();
+        }
+
+        [Route("task/{id}/delete")]
+        public ActionResult TaskDelete(int id)
+        {
+            return View();
+        }
+    }
+}
